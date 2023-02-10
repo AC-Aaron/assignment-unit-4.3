df24ac6e123e730c68835f3f51aece8671001bdb
Aaron Aguilar-Chavez <oyorom8@gmail.com>
2023-02-10 15:38:17 -0600

    added first and second function, tested both, good to go

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,45 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(newItem){//function with param1/argument
+    basket.push(newItem);//adds new items to array 'basket
+    return true;//returns true after each item added. 
+}
+
+console.log(addItem('bread'));//adding first new item
+
+console.log(basket);//checking array after adding the new item
+
+console.log(addItem('milk'));//adding second item
+
+console.log(addItem('bacon','tomato', 'lettuce','cheese')); //adding more iteams
+//would only let me add one at a time. :(
+
+/* First function called "addIteam" must: 
+    -take an input parameter for a string item
+    -add the new item to the global array basket
+    -return true indicating the item was added*/
+
+    console.log('***NEXT FUNCTION***');
+
+    
+    
+    function listItems(){
+        for(item of basket){
+            console.log(item);
+        }//using 'for of' loop to log basket items individually on each line
+    }// end listItems function
+   
+    console.log(listItems());//
+
+    
+
+/*  Create a function called listItems. It should:
+        -loop over the items in the basket array
+        -console.log each individual item on a new line
+
+    Create a function called empty. It should:
+        -reset the basket to an empty array*/
